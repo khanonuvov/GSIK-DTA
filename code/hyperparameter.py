37f20331c40df74e140e79747b3a65ae0be59853
MD. Youshuf Khan Rakib <59321214+khanonuvov@users.noreply.github.com>
2026-01-22 20:03:14 +0800

    Update hyperparameter.py

--- code/hyperparameter.py
+++ code/hyperparameter.py
@@ -5,7 +5,7 @@ class HyperParameter:
     def __init__(self):
         self.current_time = datetime.now().strftime('%b%d_%H-%M-%S')
         self.data_root = './KANPM-DTA/datasets'
-        self.dataset = 'davis' #kiba, metz
+        self.dataset = 'davis' # davis, kiba, metz, bindingDB
         self.running_set = 'warm' #novel-drug, novel-prot, novel-pair
         
         self.mol2vec_dir = './KANPM-DTA/pretrained/{self.dataset}/{self.dataset}_chem_pretrained.pkl'
